a2497067130ab8878952694a69905b7f2ff74667
Ronen Goldenber <ronen.goldenber@gmail.com>
2015-08-21 04:18:46 +0000

    Add support for linphone 1414 files

--- device/linphone/ln1414.py
+++ device/linphone/ln1414.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 import linphone
 import logging
+import os
 import signal
 import time
 class Phone1414:
@@ -15,7 +16,10 @@ class Phone1414:
 		linphone.set_log_handler(self.log_handler)
 		self.core = linphone.Core.new(callbacks, None, None)
 		self.core.max_calls = 1
+		self.core.start_dtmf_stream()
 		self.core.echo_cancellation_enabled = False
+		self.core.use_info_for_dtmf = False
+		self.core.use_rfc2833_for_dtmf = True
 		# Only enable PCMU and PCMA audio codecs
 		for codec in self.core.audio_codecs:
 			print codec.mime_type
@@ -33,16 +37,21 @@ class Phone1414:
 		if state == linphone.CallState.IncomingReceived:
 			params = core.create_call_params(call)
 			core.accept_call_with_params(call, params)
+			os.system("/home/pi/1414/calls/on_incoming_call.sh {0}".format(call.remote_params.get_custom_header('X-call-number')))
  
+		if state == linphone.CallState.CallEnd:
+			print "Call ended"
+			os.system("/home/pi/1414/calls/on_call_hangup.sh")
+
 	def configure_sip_account(self):
 		# Configure the SIP account
 		proxy_cfg = self.core.create_proxy_config()
-		proxy_cfg.identity = 'sip:<username>@<domain>'
+		proxy_cfg.identity = 'sip:16503383032@tmusqa.com'
 		proxy_cfg.server_addr = '192.168.1.110'
 		proxy_cfg.register_enabled = True
 		proxy_cfg.expires = 3600
 		self.core.add_proxy_config(proxy_cfg)
-		auth_info = self.core.create_auth_info('<username>', None, '<password>', None, None, '<domain>')
+		auth_info = self.core.create_auth_info('16503383032', None, 'choochee1', None, None, 'tmusqa.com')
 		self.core.add_auth_info(auth_info)
  
 	def run(self):
