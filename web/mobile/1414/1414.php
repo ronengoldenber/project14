08ce2e5d08f0a1f94534cfb823b4cd84b76de0db
Yiftah Golan <yiftah.golan@gmail.com>
2015-10-18 21:02:59 -0700

    Add support for mobile website

---
+++ web/mobile/1414/1414.php
@@ -0,0 +1,197 @@
+<?php
+include 'global.php';
+function js_validate_fname() {
+	echo 'function validate_fname(fname) {' . PHP_EOL;
+	echo '	var re = /^[a-zA-Z]{2,15}$/i;' . PHP_EOL;
+	echo '	var is_fname = re.test(fname);' . PHP_EOL;
+	echo '	return is_fname;' . PHP_EOL;
+	echo '}' . PHP_EOL;
+	return true;
+}
+function js_check_fname($form) {
+	echo 'function check_fname() {' . PHP_EOL;
+	echo '	var fname = document.forms["' . $form . '"]["fname"].value;' . PHP_EOL;
+	echo '	var div = document.getElementById("subdivfname");' . PHP_EOL;
+	echo '	var color = "red";' . PHP_EOL;
+	echo '  if (fname == "") { color = "gray"; }' . PHP_EOL;
+	echo '	if (fname != "" && validate_fname(fname)) { color = "green"; }' . PHP_EOL;
+	echo '  div.innerHTML = "<font size=2>&nbsp;</font>";' . PHP_EOL;
+	echo '	if(color == "red") {' . PHP_EOL;
+	echo '		div.innerHTML = "<font size=2 color=red><b>First name must be 2 to 15 letters</b></font>";' . PHP_EOL;
+	echo '	}' . PHP_EOL;
+	echo '	document.getElementById("fname").style.borderColor = color;' . PHP_EOL;
+	echo '}' . PHP_EOL;
+	return true;
+}
+function js_validate_lname() {
+	echo 'function validate_lname(lname) {' . PHP_EOL;
+	echo '  var re = /^[a-zA-Z]{2,15}$/i;' . PHP_EOL;
+	echo '  var is_lname = re.test(lname);' . PHP_EOL;
+	echo '  return is_lname;' . PHP_EOL;
+	echo '}' . PHP_EOL;
+	return true;
+}
+function js_check_lname($form) {
+	echo 'function check_lname() {' . PHP_EOL;
+	echo '  var lname = document.forms["' . $form . '"]["lname"].value;' . PHP_EOL;
+	echo '  var div = document.getElementById("subdivlname");' . PHP_EOL;
+	echo '  var color = "red";' . PHP_EOL;
+	echo '  if (lname == "") { color = "gray"; }' . PHP_EOL;
+	echo '  if (lname != "" && validate_lname(lname)) { color = "green"; }' . PHP_EOL;
+	echo '	div.innerHTML = "<font size=2>&nbsp;</font>";' . PHP_EOL;
+	echo '	if(color == "red") {' . PHP_EOL;
+	echo '		div.innerHTML = "<font size=2 color=red><b>Last name must be 2 to 15 letters</b></font>";' . PHP_EOL;
+	echo '	}' . PHP_EOL;
+	echo '  document.getElementById("lname").style.borderColor = color;' . PHP_EOL;
+	echo '}' . PHP_EOL;
+	return true;
+}
+function html_field($title, $title_font_size, $field, $input, $nbsp) {
+	echo '<div id="subdiv" align="left"><font size=' . $title_font_size . ' color=black>' . $title . '</font></div>' . PHP_EOL;
+	echo '<div id="subdiv" align="left"><input id="' . $field . '" name="' . $field . '"' ;
+	echo ' onchange="check_' . $field . '();" onkeyup="check_' . $field . '();"';
+	echo ' onpaste="check_' . $field . '();" oninput="check_' . $field . '();" onchange="check_' . $field . '();" maxlength=40 size=40 type=' . $input . '></font></div>' . PHP_EOL;
+	echo '<div id="subdiv' . $field . '"><font size=2>' . $nbsp . '</font></div>' . PHP_EOL;
+	return true;
+}
+function js_validate_email() {
+	echo 'function validate_email(email) {' . PHP_EOL;
+	echo '	var re = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/i;' . PHP_EOL;
+	echo '	var is_email = re.test(email);' . PHP_EOL;
+	echo '	return is_email;' . PHP_EOL;
+	echo '}' . PHP_EOL;
+}
+function js_check_email($form) { 
+	echo 'function check_email() {' . PHP_EOL;
+	echo '	var email = document.forms["' . $form . '"]["email"].value;' . PHP_EOL;
+	echo '	var div = document.getElementById("subdivemail");' . PHP_EOL;
+	echo '	var color = "red";' . PHP_EOL;
+	echo '  if (email == "") { color = "gray"; }' . PHP_EOL;
+	echo '	if (email != "" && validate_email(email)) { color = "green"; }' . PHP_EOL;
+	echo '	div.innerHTML = "<font size=2>&nbsp;</font>";' . PHP_EOL;
+	echo '	if(color == "red") {' . PHP_EOL;
+	echo '		div.innerHTML = "<font size=2 color=red><b>Please enter valid email</b></font>";' . PHP_EOL;
+	echo '	}' . PHP_EOL;
+	echo '	document.getElementById("email").style.borderColor = color;' . PHP_EOL;
+	echo '}' . PHP_EOL;
+	return true;
+}
+function js_validate_password() {
+	echo 'function validate_password(password) {' . PHP_EOL;
+	echo '	var re = /^((?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[@#$%!]).{6,20})$/i;' . PHP_EOL;
+	echo '	var is_password = re.test(password);' . PHP_EOL;
+	echo '	return is_password;' . PHP_EOL;
+	echo '}' . PHP_EOL;
+	return true;
+}
+function js_check_password($form) {
+	echo 'function check_password() {' . PHP_EOL;
+	echo '	var password = document.forms["' . $form . '"]["password"].value;' . PHP_EOL;
+	echo '	var div = document.getElementById("subdivpassword");' . PHP_EOL;
+	echo '	var color = "red";' . PHP_EOL;
+	echo '	if (password == "") { color = "gray"; }' . PHP_EOL;
+	echo '	if (password != "" && validate_password(password)) { color = "green"; }' . PHP_EOL;
+    echo '	div.innerHTML = "<font size=2>&nbsp;</font>";' . PHP_EOL;
+	echo '	if(color == "red") {' . PHP_EOL;
+	echo '		div.innerHTML = "<font size=2 color=red><b>Password must contains digit,<br> lowercase, uppercase, special symbols,<br> length between 6 and 20</b></font>";' . PHP_EOL;
+	echo '	}' . PHP_EOL;
+	echo '	document.getElementById("password").style.borderColor = color;' . PHP_EOL;
+	echo '}' . PHP_EOL;
+	return true;
+}
+function js_validate($form) {
+	echo 'function validate() {' . PHP_EOL;
+	echo '	var email = document.forms["' . $form . '"]["email"].value;' . PHP_EOL;
+	echo '	var password = document.forms["' . $form . '"]["password"].value;' . PHP_EOL;
+	echo '	var div = document.getElementById("subdivemail");' . PHP_EOL;
+	echo '	var isEmail = validate_email(email);' . PHP_EOL;
+	echo '	var isPassword = validate_password(password);' . PHP_EOL;
+	echo '	var divPassword = document.getElementById("subdivpassword");' . PHP_EOL;
+	echo '	if (!isEmail) {' . PHP_EOL;
+	echo '		div.innerHTML = "<font size=2 color=red><b>Please enter valid email</b></font>";' . PHP_EOL;
+	echo '		return false;' . PHP_EOL;
+	echo '	}' . PHP_EOL;
+	echo '	if (!isPassword) {' . PHP_EOL;
+	echo '		divPassword.innerHTML = "<font size=2 color=red><b>Password must contains digit, lowercase, uppercase, special symbols, length between 6 and 20</font>";' . PHP_EOL;
+	echo '		return false; ' . PHP_EOL;
+	echo '	}' . PHP_EOL;
+	echo '	return true;' . PHP_EOL;
+	echo '}' . PHP_EOL;
+}
+function html_tos_pp() {
+	echo '<div id="subdiv"><font size=1 color=black> By clicking Join now<br>You agree to 1414\'s <a href=http://1414intl.com/terms>Terms and Conditions</a> and <a href=http://1414intl.com/privacy>Privacy Policy</a></font></div>' . PHP_EOL;
+	return true;
+}
+function screen_size() {
+	echo '<script>' . PHP_EOL;
+	echo '	function checkImg() {' . PHP_EOL;
+	echo '		var width = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;' . PHP_EOL;
+	echo '		var height = window.innerHeight || document.documentElement.clientHeight || document.body.clientHeight;' . PHP_EOL;
+	echo '//	if(height > 700) {' . PHP_EOL;
+	echo '//		document.getElementById("html_join").style.backgroundImage = "url(\'http://1414intl.com/1414/images/phone_booth.jpg\')";' . PHP_EOL;
+	echo '//	}' . PHP_EOL;
+	echo '//	if(height < 700) {' . PHP_EOL;
+	echo '//		document.getElementById("html_join").style.backgroundImage = "url(\'http://1414intl.com/1414/images/join.jpg\')";' . PHP_EOL;
+	echo '//	}' . PHP_EOL;
+	echo '}' . PHP_EOL;
+	echo 'checkImg();' . PHP_EOL;
+	echo '</script>' . PHP_EOL;
+	return true;
+}
+function validate_fname($fname) {
+	if(!isset($fname)) {
+		logmsg(LOG_DEBUG, 'The first name is not set ');
+		return 'The first name is not set';
+	}
+	if(strlen($fname) < 2) { 
+		logmsg(LOG_DEBUG, 'The first name is too short ');
+		return 'The first name is too short';
+	}
+	if(strlen($fname) > 15) { 
+		logmsg(LOG_DEBUG, 'The first name is too long ');
+		return 'The first name is too long';
+	}
+	return '';
+}
+function validate_lname($lname) {
+	if(!isset($lname)) {
+		logmsg(LOG_DEBUG, 'The last name is not set ');
+		return 'The last name is not set';
+	}
+	if(strlen($lname) < 2) {
+		logmsg(LOG_DEBUG, 'The last name is too short ');
+		return 'The last name is too short (less than 2 characters)';
+	}
+	if(strlen($lname) > 15) {
+		logmsg(LOG_DEBUG, 'The last name is too long ');
+		return 'The last name is too long (more than 15 characters)';
+	}
+	return '';
+}
+function validate_email($email) {
+	if(!isset($email)) {
+		logmsg(LOG_DEBUG, 'The email is invalid it is not set ');
+		return 'The email is invalid it is not set';
+	}
+	if (filter_var($email, FILTER_VALIDATE_EMAIL)) {
+		return '';
+	}
+	logmsg(LOG_DEBUG, 'The email invalid ');
+	return 'The email invalid';
+}
+function validate_password($password) {
+	if(!isset($password)) { 
+		logmsg(LOG_DEBUG, 'The password is invalid it is not set ');
+		return 'The password is invalid it is not set';
+	}
+	if(strlen($password) < 8) {
+		logmsg(LOG_DEBUG, 'The password length cannot be less than 8 characters ');
+		return 'The password length cannot be less than 8 characters';
+	}
+	if(strlen($password) > 20) {
+		logmsg(LOG_DEBUG, 'The password length cannot be more than 20 characters '); 
+		return 'The password length cannot be more than 20 characters';
+	}
+	return '';
+}
+?>
---
+++ web/mobile/1414/db.php
@@ -0,0 +1,125 @@
+<?php
+include_once 'global.php';
+function add_user($link, $fname, $lname, $email, $username, $type) {
+	$fname = printable($fname);
+	$lname = printable($lname);
+	$email = printable($email);
+	$username = printable($username);
+	if(($stmt = sql_query($link, 'DELETE FROM config_device WHERE username = ?', 'd', array($username)))) {
+		exit_stmt($stmt);
+	}
+	$user_id = get_user_id_by_email($link, $email);
+	if(!$user_id || strlen($user_id) <= 0) {
+		$stmt = sql_query($link, 'INSERT INTO config_user (tenant_id, fname, lname, email, language, type) VALUES (240000001, ?, ?, ?, 0, ?);', 'ssss', array($fname, $lname, $email, $type));
+		exit_stmt($stmt);
+		$user_id = get_user_id_by_email($link, $email);
+	}
+	$stmt = sql_query($link, 'INSERT INTO config_device (user_id, username, ha1) VALUES (?, ?, "aa");', 'ss', array($user_id, $username));
+	exit_stmt($stmt);
+	$stmt = sql_query($link, 'UPDATE config_device set ha1 = md5(concat(?, ":", ?, ":", ?)) WHERE username = ?', 'ssss', array($username, 'tmusqa.com', 'choochee1', $username));
+	exit_stmt($stmt);
+	return true;
+}
+function get_user_id_by_email($link, $email) {
+	$user_id = '';
+	if(($stmt = sql_query($link, 'SELECT user_id FROM config_user WHERE email = ?', 's', array($email)))) {
+		mysqli_stmt_bind_result($stmt, $user_id);
+		exit_stmt($stmt);
+	}
+	return $user_id;
+}
+function get_user_type_by_email($link, $email) {
+	$type = '';
+	if(($stmt = sql_query($link, 'SELECT `type` FROM `config_user` WHERE `email` = ?', 's', array($email)))) {
+		mysqli_stmt_bind_result($stmt, $type);
+		exit_stmt($stmt);
+	}
+	return $type;
+}
+function get_device_id_by_email($link, $email) { 
+	$device_id = '';
+	$query = 'SELECT d.`device_id` FROM `config_user` s JOIN `config_device` d ON s.`user_id` = d.`user_id` WHERE s.email = ? AND d.`device_type` = 1 LIMIT 1';
+	if(($stmt = sql_query($link, $query, 's', array($email)))) {
+		mysqli_stmt_bind_result($stmt, $device_id);
+		exit_stmt($stmt);
+	}
+	logmsg(LOG_DEBUG, 'the device id found [' . $device_id . '] by email [' . $email . '] '); 
+	return $device_id;
+}
+function get_device_type_by_username($link, $username) {
+	$device_type = '';
+	$query = 'SELECT `device_type` FROM `config_device` WHERE username = ? LIMIT 1';
+	if(($stmt = sql_query($link, $query, 's', array($username)))) {
+		mysqli_stmt_bind_result($stmt, $device_type);
+		exit_stmt($stmt);
+	}
+	logmsg(LOG_DEBUG, 'the device type found [' . $device_type . '] by username [' . $username . '] ');
+	return $device_type;
+}
+function get_state_device_id_by_username($link, $username) {
+	$state_device_id = '';
+	if(($stmt = sql_query($link, 'SELECT sd.state_device_id FROM config_device d JOIN state_device sd ON d.device_id = sd.device_id WHERE d.username = ?', 's', array($username)))) {
+		mysqli_stmt_bind_result($stmt, $state_device_id);
+		exit_stmt($stmt);
+	}
+	logmsg(LOG_DEBUG, 'getting state device id [' . $state_device_id . '] by username [' . $username . '] ' ); 
+	return $state_device_id;
+}
+function get_country_by_username($link, $username) { 
+	$country = '1';
+	if(($stmt = sql_query($link, 'SELECT `country` FROM config_device WHERE username = ?', 's', array($username)))) {
+		mysqli_stmt_bind_result($stmt, $country);
+		exit_stmt($stmt);
+	}
+	logmsg(LOG_DEBUG, 'getting country [' . $country . '] by user name [' . $username . '] '); 
+	return $country;
+}
+function get_ha1_by_username($link, $username) {
+	$ha1 = '';
+	if(($stmt = sql_query($link, 'SELECT ha1 FROM config_device WHERE username = ?', 's', array($username)))) {
+		mysqli_stmt_bind_result($stmt, $ha1);
+		exit_stmt($stmt);
+	}
+	return $ha1;
+}
+function get_nonce_by_username($link, $username) {
+	$nonce = '';
+	if(($stmt = sql_query($link, 'SELECT sd.nonce FROM config_device d JOIN state_device sd ON d.device_id = sd.device_id WHERE d.username = ?', 's', array($username)))) {
+		mysqli_stmt_bind_result($stmt, $nonce);
+		exit_stmt($stmt);
+	}
+	return $nonce;
+}
+function get_apikey_by_username($link, $username) {
+	$apikey = '';
+	if(($stmt = sql_query($link, 'SELECT sd.apikey FROM config_device d JOIN state_device sd ON d.device_id = sd.device_id WHERE d.username = ?', 's', array($username)))) {
+		mysqli_stmt_bind_result($stmt, $apikey);
+		exit_stmt($stmt);
+	}
+	return $apikey;
+}
+function get_match_username($link, $src) {
+	$dst = '';
+	logmsg(LOG_DEBUG, 'Try to match username for src [' . $src . '] ');
+	if(($stmt = sql_query($link, 'SELECT `dst_username` FROM config_device_group WHERE src_username = ?', 's', array($src)))) {
+		mysqli_stmt_bind_result($stmt, $dst);
+		exit_stmt($stmt);
+	}
+	return $dst;
+}
+function get_clicmd($link, $username) {
+	$query = 'SELECT sc.`cmd_id`, sc.`cmd` FROM `config_device` d JOIN `state_cmd` sc ON d.`device_id` = sc.`device_id` WHERE d.`username` = ? LIMIT 1';
+	if(!($stmt = sql_query($link, $query, 's', array($username)))) {
+		return true;
+	}
+	mysqli_stmt_bind_result($stmt, $cmd_id, $cmd);
+	exit_stmt($stmt);
+	if (isset($cmd) && isset($cmd_id) && $cmd != '' && $cmd_id != '' && strlen($cmd) > 0 && strlen($cmd_id) > 0) {
+		sql_query($link, 'DELETE FROM `state_cmd` WHERE `cmd_id` = ?', 's', array($cmd_id));
+		exit_stmt($stmt);
+	}
+	logmsg(LOG_DEBUG, 'The command found is [' . $cmd . '] ');
+	return $cmd;
+}
+
+
